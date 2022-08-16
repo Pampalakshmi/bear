2e56d3f5598e65e086199c65a89ee0c0a8d9fd1f
Pampalakshmi <109756314+Pampalakshmi@users.noreply.github.com>
2022-08-16 06:03:23 +0530

    Add files via upload

---
+++ send.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main()
+{
+    int i, j, k = 0, l = 1, n = 5;
+    for (i=1; i<=n; i++)
+    {
+        for (j=1; j<=i; j++, k++, l++)
+        {
+            if (k % 2 == 0)
+            {
+                printf("%4c", (l+96));
+            }
+            else
+            {
+                printf("%4c",(l+64));
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+    
